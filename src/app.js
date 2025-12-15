c39c9bfbf5bbeadb2c1c5cbef89629bad70f1282
shra213 <sc0171573@gmail.com>
2025-12-15 23:55:36 +0530

    saaa

--- src/app.js
+++ src/app.js
@@ -17,12 +17,12 @@ import 'dotenv/config';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-        methods: ["GET", "POST"],
-        credentials: true
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
+
 
 
 app.use(cors({
